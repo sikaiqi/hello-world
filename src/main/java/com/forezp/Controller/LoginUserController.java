2bef21613dde7af5de28d339930ac2f9fb23d913
skq <sikaiqi1992@163.com>
2019-01-25 18:02:51 +0800

    add redis、swagger test

--- src/main/java/com/forezp/Controller/LoginUserController.java
+++ src/main/java/com/forezp/Controller/LoginUserController.java
@@ -2,21 +2,56 @@ package com.forezp.Controller;
 
 import com.forezp.Bean.LoginUser;
 import com.forezp.Service.LoginUserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
 
 /**
  * Created by skq on 2019/1/24.
  */
+@RequestMapping("/user")
 @RestController
 public class LoginUserController {
     @Autowired
     private LoginUserService loginUserService;
 
-    @GetMapping("/{username}")
-    public LoginUser getLoginUser(@PathVariable("username") String username){
-        return loginUserService.findLoginUserByName(username);
+    @ApiOperation(value="用户列表",notes="用户列表")
+    @GetMapping(value={""})
+    public List<LoginUser> getUsers(){
+        List<LoginUser> users = loginUserService.findAll();
+        return users;
     }
+
+    @ApiOperation(value="创建用户",notes="创建用户")
+    @PostMapping(value="")
+    public LoginUser postUser(@RequestBody LoginUser loginUser){
+        return loginUserService.saveLoginUser(loginUser);
+    }
+
+    @ApiOperation(value="获取用户信息",notes="根据url的id来获取详细信息")
+    @GetMapping(value="/{id}")
+    public LoginUser getLoginUser(@PathVariable Long id){
+        return loginUserService.findLoginUserById(id);
+    }
+
+    @ApiOperation(value="更新信息",notes="根据url的id来指定更新用户信息")
+    @PutMapping(value="/{id}")
+    public LoginUser putLoginUser(@PathVariable Long id,@RequestBody LoginUser user){
+        LoginUser loginUser = new LoginUser();
+        loginUser.setUsername(user.getUsername());
+        loginUser.setPassword(user.getPassword());
+        loginUser.setId(id);
+        return loginUserService.updateLoginUser(loginUser);
+    }
+
+    @ApiOperation(value="删除用户",notes="根据url的id来指定删除用户信息")
+    @DeleteMapping(value="/{id}")
+    public String deleteUser(@PathVariable Long id){
+       loginUserService.deleteLoginUser(id);
+       return "success";
+    }
+
 }
--- src/main/java/com/forezp/Service/LoginUserService.java
+++ src/main/java/com/forezp/Service/LoginUserService.java
@@ -4,6 +4,9 @@ import com.forezp.Bean.LoginUser;
 import com.forezp.Dao.LoginUserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.rmi.log.LogInputStream;
+
+import java.util.List;
 
 /**
  * Created by skq on 2019/1/24.
@@ -16,4 +19,24 @@ public class LoginUserService {
     public LoginUser findLoginUserByName(String username){
         return userRepository.findByUsername(username);
     }
+
+    public List<LoginUser> findAll(){
+        return userRepository.findAll();
+    }
+
+    public LoginUser saveLoginUser(LoginUser loginUser){
+        return userRepository.save(loginUser);
+    }
+
+    public LoginUser findLoginUserById(long id){
+        return userRepository.findById(id).get();
+    }
+    public LoginUser updateLoginUser(LoginUser loginUser){
+        return userRepository.saveAndFlush(loginUser);
+    }
+    public void deleteLoginUser(long id){
+        userRepository.deleteById(id);
+    }
+
+
 }
